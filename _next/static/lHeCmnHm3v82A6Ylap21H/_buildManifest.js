2553fe9bc66e6acd7e5b38c6b4055d9e383cb7e6
Eunseo Seol <147463468+eunseoseol@users.noreply.github.com>
2023-11-27 21:56:12 +0900

    Delete _next/static directory

--- _next/static/lHeCmnHm3v82A6Ylap21H/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-771043b22f1550be.js"],"/_error":["static/chunks/pages/_error-b5ee443ea3f1b36c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
