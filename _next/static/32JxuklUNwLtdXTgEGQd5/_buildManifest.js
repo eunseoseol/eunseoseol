0afc9bb183ecc266735d957bb20e28ea0fb01f0e
Eunseo Seol <147463468+eunseoseol@users.noreply.github.com>
2023-11-27 22:35:26 +0900

    Add files via upload

---
+++ _next/static/32JxuklUNwLtdXTgEGQd5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-416ebeb99bbfecd2.js"],"/_error":["static/chunks/pages/_error-b5ee443ea3f1b36c.js"],"/head":["static/chunks/pages/head-034162fe20ab5787.js"],sortedPages:["/","/_app","/_error","/head"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
