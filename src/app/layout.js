ecb81fdca7441c73d601c7cd67f3697d34e83605
Eunseo Seol <147463468+eunseoseol@users.noreply.github.com>
2023-12-16 10:36:09 +0900

    Add files via upload

---
+++ src/app/layout.js
@@ -0,0 +1,23 @@
+"use client";
+import Navbar from "./components/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { AuthContextProvider } from "./context/AuthContext";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthContextProvider>
+          {children}
+          <Navbar />
+
+        </AuthContextProvider>
+      </body>
+    </html>
+  );
+}
