cf771a55842353b68e8f708fb67c0abf444f6750
Eunseo Seol <147463468+eunseoseol@users.noreply.github.com>
2023-12-16 11:05:34 +0900

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,6 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <AuthContextProvider>
           {children}
-          <Navbar />
 
         </AuthContextProvider>
       </body>
