8d272c7f0f254ad0c265db60918e83f06338d8fa
Eunseo Seol <147463468+eunseoseol@users.noreply.github.com>
2024-01-01 19:48:22 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,75 +1,129 @@
-// Page.js
 "use client";
 
-import React, { useState } from "react";
+
+import React, { useState, useEffect } from "react";
 
 export default function Page() {
-  // 페이지 상태를 관리하는 useState. 초기 상태는 'Red Pill'.
-  const [pageState, setPageState] = useState('Red Pill');
+  const [isMobile, setIsMobile] = useState(false);
 
-  const handleRedPillClick = () => {
-    setPageState('Blue Pill');
+  const hoverStyle = {
+    maxWidth: '150px',
+    marginBottom: '20px',
   };
+  const mobilehoverStyle = {
+    maxWidth: '80px',
+    marginBottom: '10px',
+  };
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // 768px 미만을 모바일로 간주합니다.
+    };
 
-  const handleBluePillClick = () => {
-    setPageState('Red Pill');
-  };
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+ 
+//https://apps.apple.com/us/app/edith-space-assistant/id6450296741
+  // 이곳에 다른 useEffect 훅들 및 로직 추가...
+
+  if (isMobile) {
+    return (
+      <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+  
+      <video autoPlay loop muted playsInline style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', objectFit: 'cover', zIndex: -1 }}>
+  
+      <source src="/background.mp4" type="video/mp4" />
+    </video>
+  
+  <div style={{ position: 'relative', zIndex: 1, width: '100%', paddingTop: '20px', paddingBottom: '100px', height: 'calc(100vh - 20px)',  display: 'flex', flexDirection: 'column', alignItems: 'flex-start', alignItems: 'center' }}>
+  
+      
+            <main className="p-4" style={{ height: '100vh' }}>
+  
+          {/* 사용자가 로그인되어 있으면 Welcome 메시지를 표시하고, 그렇지 않으면 Home Page를 표시합니다. */}
+          <div style={{ position: 'relative', zIndex: 1, paddingBottom: '40px', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',marginTop: '-50px' // Move the whole block up by 50px
+  }}>
+  
+  <a   href="https://medium.com/@eunseoseol"
+            >
+              <img src="/book.png" alt="Book" style={mobilehoverStyle} />
+            </a>
+  
+            <h1 style={{ fontSize: '16px', fontWeight: 'bold', color: 'white' }}>
+            Galactic Empire
+            </h1>
+            <h1 style={{ fontSize: '16px', fontWeight: 'bold', color: 'white' }}>
+       Respect and enjoy the peace
+
+            </h1>
+           
+          {/* book1.png on the left */}
+         
+        
+           
+  
+          
+  
+          
+  
+        </div>
+       
+        </main>
+       
+      </div>
+  
+      </div>
+  
+    );
+  }
 
   return (
     <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
-      <video autoPlay loop muted playsInline style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', objectFit: 'cover', zIndex: -1 }}>
-        <source src="/background.mov" type="video/mp4" />
-      </video>
 
-      <main className="p-4">
-        {pageState === 'Red Pill' ? (
-          <div style={{ position: 'relative', zIndex: 1, height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            <img src="/book.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-            <button onClick={handleRedPillClick}>Red Pill</button>
-          </div>
-        ) : (
-          <div style={{ position: 'relative', zIndex: 1, width: '100%', paddingTop: '20px', height: 'calc(100vh - 20px)', overflowY: 'scroll', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', alignItems: 'center' }}>
-          <img src="/book0.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book1.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book2.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book3.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book4.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book5.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book6.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book7.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book8.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book9.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book10.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book11.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book12.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book13.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book14.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book15.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book16.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book17.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book18.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book19.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book20.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book21.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book22.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book23.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book24.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book25.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book26.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-          <img src="/book27.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-  
-  
-    <p className="cursor-pointer" onClick={handleBluePillClick}>
-   Blue Pill
-  </p>
-  
-  
-  
-  
-        </div>
+    <video autoPlay loop muted playsInline style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', objectFit: 'cover', zIndex: -1 }}>
+
+    <source src="/background.mp4" type="video/mp4" />
+  </video>
+
+<div style={{ position: 'relative', zIndex: 1, width: '100%', paddingTop: '20px', paddingBottom: '100px', height: 'calc(100vh - 20px)', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', alignItems: 'center' }}>
+
+    
+          <main className="p-4" style={{ height: '100vh' }}>
+
+        {/* 사용자가 로그인되어 있으면 Welcome 메시지를 표시하고, 그렇지 않으면 Home Page를 표시합니다. */}
+        <div style={{ position: 'relative', zIndex: 1, paddingBottom: '40px', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',marginTop: '-50px' // Move the whole block up by 50px
+}}>
+
+<a   href="https://medium.com/@eunseoseol"
+          >
+            <img src="/book.png" alt="Book" style={hoverStyle} />
+          </a>
+
+          <h1 style={{ fontSize: '48px', fontWeight: 'bold', color: 'white' }}>
+         Galactic Empire
+          </h1>
+          <h1 style={{ fontSize: '48px', fontWeight: 'bold', color: 'white' }}>
+      Respect and enjoy the peace
         
-        )}
+          </h1>
+          <div style={{ paddingBottom: '40px' }}>
+      
+
+          </div>
+
+        
+
+        
+
+      </div>
+     
       </main>
+     
     </div>
+
+    </div>
+
   );
 }
