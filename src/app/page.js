d3a419796976c115edd769e9a5cdf3087ea7063e
Eunseo Seol <147463468+eunseoseol@users.noreply.github.com>
2024-01-23 17:00:56 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -82,7 +82,7 @@ export default function Page() {
     );
 
     await tossPayments.requestPayment("카드", {
-      amount: 10000,
+      amount: 1000000,
       orderId: Math.random().toString(36).slice(2),
       orderName: "It from bit.",
       successUrl: `${window.location.origin}/api/payments`,
